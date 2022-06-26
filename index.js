93c56c20ad4dcae96c7fe7e4eb23d5656d705202
roberbaca <roberto.nicolas.baca@gmail.com>
2022-06-26 19:25:28 -0300

    Agregue roles (admin y user). Nuevo schema incluyendo el userId en la tabla de gastos

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require('cors');
 
-
 app.use( bodyParser.urlencoded( {extended: true }));
 app.use( bodyParser.json() );
 app.use( cors() );
